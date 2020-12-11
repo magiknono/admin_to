e8b73c10c9792d242ed1464919b300306730b9e0
arnaud cormier <arno.cormier@gmail.com>
2020-12-11 12:11:57 +0100

    fix login & sign_in redirect btn after log out

--- lib/admin_to_web/controllers/user_auth.ex
+++ lib/admin_to_web/controllers/user_auth.ex
@@ -81,7 +81,7 @@ defmodule AdminToWeb.UserAuth do
     conn
     |> renew_session()
     |> delete_resp_cookie(@remember_me_cookie)
-    |> redirect(to: "/")
+    |> redirect(to: "/home")
   end
 
   @doc """
