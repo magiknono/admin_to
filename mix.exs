84c3246b311e020524da7ae4a46b93992d3e250c
arnaud cormier <arno.cormier@gmail.com>
2020-12-13 21:31:02 +0100

    feat(linter): adding credo

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule AdminTo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phx_gen_auth, "~> 0.6.0"}
+      {:phx_gen_auth, "~> 0.6.0"},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
